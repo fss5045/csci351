552cb202c1d2bb6df8a6e914dfb132d29b62e353
Finn <fss5045@rit.edu>
2025-03-18 13:16:13 -0400

    basic send receive untested

--- hw3/rdt.py
+++ hw3/rdt.py
@@ -4,25 +4,37 @@ import socket
 import scapy.all as scapy
 
 
-
 def send_pkt(data, dest, seq):
     dest_ip = socket.gethostbyname(dest)
-
+    
     pkt = scapy.IP(dst=dest_ip) / seq / scapy.UDP() / data
     
-
-    reply = scapy.sr1(pkt, verbose=0)
-    return
-
-
-def recieve_pkt(pkt):
+    start_time = time.time()
+    return pkt, start_time
 
 
-    return
+def corrupt(pkt):
+    csum = 0x0
+    for b in scapy.raw(pkt[scapy.UDP]):
+        csum += b
+
+    if pkt[scapy.UDP].chksum + csum == 0xFFFF:
+        return False
+    return True
 
 
-def forward_pkt():
-    return
+def recieve_pkt(pkt, seq):
+    if corrupt(pkt) or pkt.seq != seq:
+        send_pkt(pkt)
+    elif not corrupt(pkt) and pkt.seq == seq:
+        return True
+
+
+def forward_pkt(pkt, new_dest):
+    new_dest_ip = socket.gethostbyname(new_dest)
+    pkt.dst = new_dest_ip
+
+    return pkt
 
 
 def main():
