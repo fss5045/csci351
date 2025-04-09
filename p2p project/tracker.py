37d006bfed0554311feb87e4c1bdc3bff80f77e4
Finn <fss5045@rit.edu>
2025-04-09 11:56:25 -0400

    metainfo generation

--- p2p project/tracker.py
+++ p2p project/tracker.py
@@ -1,15 +1,31 @@
 import time
 import socket
+from threading import *
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 tr_port = 9000
 s.bind(('127.0.0.1', tr_port))
+p_len = 8
+seeders = {}
+files = {}
+
+
+def generate_metainfo(name, file_len):
+    pieces = []
+    for i in range(0, file_len, 8):
+        pieces.append(i)
+    with open(f'{name}.torrent', 'w') as file:
+        file.write(f'{{url: {{name: {name}, piece_length: {p_len}, pieces: {pieces}, length: {file_len}}}}}')
+
+def discover_peer():
+    pkt, _ = s.recvfrom()
 
 
 def main():
-    while 1:
-        pkt, _ = s.recvfrom()
+    # discover_thread = Thread(target=discover_peer, daemon=True)
+    # discover_thread.start()
+    generate_metainfo('spiderman', 64)
 
 
 if __name__ == '__main__':
